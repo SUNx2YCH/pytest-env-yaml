c97ead2d0f36cd6d2833cfa8a7be90b30d44ec2a
Alexander Ivanovsky <1336594+SUNx2YCH@users.noreply.github.com>
2019-01-30 01:20:59 +0300

    Skip artifacts cleanup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pytest-env-yaml',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(exclude=('tests',)),
     entry_points={
         'pytest11': [
