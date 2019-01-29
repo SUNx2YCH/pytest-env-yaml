13eb074de4da93443c029edbcc9fdef9dd9b441c
Alexander Ivanovsky <1336594+SUNx2YCH@users.noreply.github.com>
2019-01-30 01:14:04 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pytest-env-yaml',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(exclude=('tests',)),
     entry_points={
         'pytest11': [
